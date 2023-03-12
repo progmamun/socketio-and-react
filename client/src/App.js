af177b9ff1aed2c66ca5c1772fcbc759959e3110
progmamun <mamun333089@gmail.com>
2023-03-12 19:28:38 +0600

    updating-app-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Comments from "./components/Comments";
+import Header from "./components/Header";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [selectedUserId, setSelectedUserId] = useState("");
+
+  async function fetchUsers() {
+    const result = await fetch("http://localhost:4000/users");
+    const users = await result.json();
+
+    setUsers(users);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        users={users}
+        selectedUserId={selectedUserId}
+        setSelectedUserId={setSelectedUserId}
+      />
+      <Comments selectedUserId={selectedUserId} />
     </div>
   );
 }
