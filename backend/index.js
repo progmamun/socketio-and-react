407b0d63d8fe56bb473b6c1e3da6bba186c15013
progmamun <mamun333089@gmail.com>
2023-03-19 13:57:31 +0600

    socket.io added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const { users } = require("./lib/users");
 const { listComments, createComment } = require("./lib/comments");
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 const http = require("http").Server(app);
 const cors = require("cors");
@@ -10,6 +15,14 @@ const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
 
 app.get("/users", (req, res) => {
   res.json(users);
@@ -20,8 +33,9 @@ app.get("/comments", (req, res) => {
   res.json(comments);
 });
 
-app.post("/comments", (req, res) => {
+app.post("/comments", async (req, res) => {
   const comment = createComment(req.body);
+  io.emit("new-comment", { comment });
   res.json(comment);
 });
 
